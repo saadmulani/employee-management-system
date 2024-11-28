0b200aedfea5f9cfd9fe2661347bd6040efb687e
saadmulani <108920480+saadmulani@users.noreply.github.com>
2024-11-28 22:11:18 +0530

    updated post(id) issue

--- src/main/java/com/employee/management/system/dto/DepartmentDto.java
+++ src/main/java/com/employee/management/system/dto/DepartmentDto.java
@@ -1,5 +1,6 @@
 package com.employee.management.system.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class DepartmentDto {
+    @Schema(description = "Auto-generated ID", accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
     private String deptName;
     private String location;
--- src/main/java/com/employee/management/system/dto/EmployeeDto.java
+++ src/main/java/com/employee/management/system/dto/EmployeeDto.java
@@ -2,6 +2,7 @@ package com.employee.management.system.dto;
 
 import java.util.Date;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class EmployeeDto {
-
+    @Schema(description = "Auto-generated ID", accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
     private String firstName;
     private String lastName;
